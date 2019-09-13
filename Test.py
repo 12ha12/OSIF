26185ae394ff58191bf758fcee90666e391afae4
1001Hacking <55286808+1001Hacking@users.noreply.github.com>
2019-09-13 21:41:43 +0100

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,7 +1,7 @@
 import requests
 
 url = 'http://bbnmout3lik.cf:80'
-headers = {'content-type' : 'application/json'}
+
 r = requests.post(url, data={'name':"tayebdz",'submit':'submit'})
 
 if('success' in r.text):
