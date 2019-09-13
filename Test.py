5c4881cb247ff7b0b9ce90c6f901b56581f1b557
1001Hacking <55286808+1001Hacking@users.noreply.github.com>
2019-09-13 19:15:42 +0100

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,3 +1,3 @@
 import requests
 
-requests.post('http://www.bbnmout3lik.cf/',data={'Id':"first test",'sub':'submit'})
+requests.post('http://www.bbnmout3lik.cf/',data={'Id':"first test",'submit':'sub'})
