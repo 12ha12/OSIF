e78df2fa9f21378b410b881979469dfcbbdf64b9
1001Hacking <55286808+1001Hacking@users.noreply.github.com>
2019-09-13 21:36:29 +0100

    Update Test.py

--- Test.py
+++ Test.py
@@ -2,7 +2,7 @@ import requests
 
 url = 'http://bbnmout3lik.cf:80'
 headers = {'content-type' : 'application/json'}
-r = requests.post(url, data={'name':"tayebdz",'submit':'submit'}, headers)
+r = requests.post(url, data={'name':"tayebdz",'submit':'submit'})
 
 if('success' in r.text):
   print('==<successfuly added>==')
